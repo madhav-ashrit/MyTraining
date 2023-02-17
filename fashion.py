a3e9851cd82b67e04ab37e15baf080f31008e2b8
madhav-ashrit <madhav261102@gmail.com>
2023-02-17 09:27:46 +0530

    a

--- fashion.py
+++ fashion.py
@@ -4,3 +4,5 @@ import seaborn as sns
 import tensorflow as tf 
 import keras
 tf.keras.datasets.fashion_mnist.load_data()
+(x_train, y_train), (x_test, y_test)=tf.keras.datasets.fashion_mnist.load_data()x_train.shape,y_train.shape,'***********************',x_test.shape,y_test.shape
+x_train[0]
