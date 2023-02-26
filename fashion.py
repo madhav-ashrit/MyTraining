8497929da64d155577e186a3fefec6d7fcba3c3d
madhav-ashrit <madhav261102@gmail.com>
2023-02-26 14:04:30 +0530

    d

--- fashion.py
+++ fashion.py
@@ -15,3 +15,11 @@ for i in np.random.randint(0,1000,25):
   plt.imshow(x_train[i],cmap="Greys")
   plt.axis('off')
   plt.title('{} / {}'.format(class_labels[y_train[i]],y_train[i]))
+  x_train=np.expand_dims(x_train,1)
+  x_train.ndim
+  x_test=np.expand_dims(x_test,1)
+  x_test.ndim
+  x_train=x_train/255
+x_test=x_test/255
+from sklearn.model_selection import train_test_split
+x_train,x_validation,y_train,y_validation=train_test_split(x_train,y_train,test_size=0.2,random_state=2020)
